09da9482910405a45fc3a6670a90a2687357ce54
unknown <greeshmasreekumar2000@gmail.com>
2024-08-25 17:20:36 +0530

    primary project structure created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SummerStories.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
